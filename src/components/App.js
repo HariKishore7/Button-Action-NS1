54c7d04cc26b576a512e5963b2c6582f80deaef6
HariKishore7 <harikishoret99@gmail.com>
2020-11-12 22:44:10 +0530

    Made some changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[showPara,setShowPara]=useState(false);//which takes any value like number booleasn 
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>setShowPara(!showPara)}>Toggle para</button>
+      {showPara ? (
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
